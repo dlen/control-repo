5f7d734f4d445960d394dce665a934ee463181ef
Diego Lendoiro <diego@iadbox.com>
2016-12-17 17:50:22 +0000

    repo release

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -1,12 +1,16 @@
 #
 class profile::php{
 
-	class { '::php::globals':
+	class{'::php::globals':
 		php_version => '7.0',
 	  config_root => '/etc/php/7.0',
 	}
 
-	class { '::php':
+  class{'::php::repo::debian':
+    release => 'jessie'
+  }
+
+	class{'::php':
 		manage_repos => true
     fpm          => true
 	}
