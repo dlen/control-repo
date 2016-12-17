47407f4ff661b5257a99aee8c227db5d81d282ae
Diego Lendoiro <diego@iadbox.com>
2016-12-17 14:13:27 +0000

    profile with less hiera ocultism

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -2,6 +2,23 @@ class profile::nginx {
 
   include ::nginx
 
-  create_resources('nginx::resource::server', hiera_hash('nginx::vhosts'))
-  create_resources('nginx::resource::location', hiera_hash('nginx::nginx_locations'))
+  nginx::vhost{'test-nro.greenpeace.org':
+    listen_port => '80',
+    server_name => 'test-nro.greenpeace.org',
+    access_log  => '/srv/test-nro/logs/access.log',
+    error_log   => '/srv/test-nro/logs/error.log'
+  }
+
+  nginx::location{'/':
+    server             => 'test-nro.greenpeace.org',
+    try_files          => [ '$uri', '$uri/', '/index.php?q=uri&$args' ];
+  '~\.php$':
+    server             => 'test-nro.greenpeace.org',
+    try_files          => [ '$uri', '=404' ],
+    fastcgi_split_path => '^(.+\.php)(/.+)$',
+    fastcgi_pass       => 'unix:/var/run/php5-fpm.sock',
+    fastcgi_index      => 'index.php',
+    include            => 'fastcgi_params'
+  }
+
 }
