53dfc0a4a6b86ba98e205c5070a55d4d577e89c6
Diego Lendoiro <diego@iadbox.com>
2016-12-19 20:48:20 +0100

    added location for php files

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -3,10 +3,22 @@ class profile::nginx {
   include ::nginx
 
   nginx::resource::server{'test':
+    ensure      => present,
     listen_port => '80 default_server',
     server_name => 'test.cloud',
     access_log  => '/srv/test-nro/logs/access.log',
     error_log   => '/srv/test-nro/logs/error.log'
+  }
+
+  nginx::resource::location{'php':
+    ensure             => present,
+    server             => "test.cloud test",
+    location           => '~ \.php$',
+    try_files          => [ '$uri', '=404' ]
+    fastcgi            => "unix:/var/run/php5-fpm.sock",
+    fastcgi_index      => 'index.php',
+    fastcgi_split_path => '^(.+\.php)(/.+)$',
+    include            => 'fastcgi_params'
   }
 
 }
