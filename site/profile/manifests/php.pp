7d90802aa796306606f790a0cccca321b4811b9b
Diego Lendoiro <diego@iadbox.com>
2016-12-19 21:42:27 +0100

    still need ordering in php module

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -4,11 +4,11 @@ class profile::php{
 	class{'::php::globals':
 		php_version => '7.0',
 	  config_root => '/etc/php/7.0'
-	}
+	}->
 
   class{'::php::repo::debian':
     release => 'jessie'
-  }
+  }->
 
 	class{'::php':
 		manage_repos => true,
