13ec244952eb9479e26f7061c8a5f112e3ca9021
Diego Lendoiro <diego@iadbox.com>
2016-12-19 21:15:39 +0100

    updated regardless documentation -_-

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -2,20 +2,20 @@ class profile::nginx {
 
   include ::nginx
 
-  nginx::resource::server{'test':
+  nginx::resource::vhost{'test':
     ensure      => present,
     listen_port => '80 default_server',
     server_name => 'test.cloud',
     access_log  => '/srv/test-nro/logs/access.log',
     error_log   => '/srv/test-nro/logs/error.log',
-    www_root    => '/srv/test-nro/html
+    www_root    => '/srv/test-nro/html'
   }
 
   nginx::resource::location{'php':
     ensure             => present,
     server             => "test.cloud test",
     location           => '~ \.php$',
-    try_files          => [ '$uri', '=404' ]
+    try_files          => [ '$uri', '=404' ],
     fastcgi            => "unix:/var/run/php5-fpm.sock",
     fastcgi_index      => 'index.php',
     fastcgi_split_path => '^(.+\.php)(/.+)$',
