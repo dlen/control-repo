224f0cb804a4782be5cb401213c84f7c3601c7c1
Diego Lendoiro <diego@iadbox.com>
2016-12-19 20:37:57 +0100

    default server

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -2,9 +2,9 @@ class profile::nginx {
 
   include ::nginx
 
-  nginx::vhost{'test-nro.greenpeace.org':
-    listen_port => '80',
-    server_name => 'test-nro.greenpeace.org',
+  nginx::vhost{'test':
+    listen_port => '80 default_server',
+    server_name => 'test.cloud',
     access_log  => '/srv/test-nro/logs/access.log',
     error_log   => '/srv/test-nro/logs/error.log'
   }
