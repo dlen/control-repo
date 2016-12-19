81d3ef42da90c54608795cb6ae1746d86f43851d
Diego Lendoiro <diego@iadbox.com>
2016-12-19 21:35:54 +0100

    reverting params

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -18,8 +18,7 @@ class profile::nginx {
     try_files          => [ '$uri', '=404' ],
     fastcgi            => "unix:/var/run/php5-fpm.sock",
     fastcgi_split_path => '^(.+\.php)(/.+)$',
-    include            => 'fastcgi_params',
-    fastcgi_param      => [ 'SCRIPT_FILENAME $document_root$fastcgi_script_name' ]
+    include            => ['fastcgi_params']
   }
 
 }
