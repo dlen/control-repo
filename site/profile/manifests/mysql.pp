d6edf9c0f6de936c53059217cbd7c62ea8ac462f
Diego Lendoiro <diego@iadbox.com>
2016-12-20 22:24:53 +0100

    initial mysql profile and data

---
+++ site/profile/manifests/mysql.pp
@@ -0,0 +1,20 @@
+class profile::mysql{
+
+  class{ '::mysql::server':
+    root_password           => lookup(mysql::root_password),
+    remove_default_accounts => true,
+		override_options        => {
+			'mysqld' => {
+				'bind-address' => '0.0.0.0'
+			}
+		}
+  }
+
+  mysql::db { 'test':
+    user     => lookup(mysql::test_username),
+    password => lookup(mysql::test_password),
+    host     => '%',
+    grant    => ['SELECT', 'UPDATE'],
+  }
+
+}
