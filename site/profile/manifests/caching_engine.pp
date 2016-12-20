a64b92cae59d7e4e49a847ef37d7de65498c0963
Diego Lendoiro <diego@iadbox.com>
2016-12-20 19:57:48 +0100

    added backends and directors for static setup remove cookies and dont cache admin panel

--- site/profile/manifests/caching_engine.pp
+++ site/profile/manifests/caching_engine.pp
@@ -1,5 +1,22 @@
 #
 class profile::caching_engine{
 
+  class{ 'varnish':
+    varnish_listen_port => '80',
+    varnish_storage_size => '1G'
+  }
+
+  class{'varnish::vcl': }
+
+  varnish::probe{ 'health_check': url => '/favicon.ico', interval => '3s' }
+
+  $backends = lookup(wordpress::backends)
+
+  $backends.each |$backend, $ip| {
+    varnish::backend{ "%{$backend}": host => "%{$ip}", port => '80', probe => 'health_check' }
+  }
+
+  $backend_names = $backends.map |$backend| { $backend[0] }
+  varnish::director{ 'wordpress_backends': backends => $backend_names }
 
 }
